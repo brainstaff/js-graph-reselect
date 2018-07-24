68e1f44f2aa2be23dc6e16992a1647ed5c9d2fab
Markin Igor <markin.io210@gmail.com>
2018-07-24 13:39:52 +0300

    Add tests for filtering.

--- test/graphReselect.test.js
+++ test/graphReselect.test.js
@@ -1,11 +1,17 @@
 import { fromJS, Map } from 'immutable';
-import {generateGraphSelector} from '../src';
 import { expect } from 'chai';
+import {generateGraphSelector} from '../src';
 
 let state = new Map({
-  users: Map(),
-  contacts: Map(),
-  phones: Map()
+  users: Map({
+    entities: Map()
+  }),
+  contacts: Map({
+    entities: Map()
+  }),
+  phones: Map({
+    entities: Map()
+  })
 });
 
 
@@ -28,83 +34,126 @@ const phones = [
 ];
 
 state = state
-  .mergeIn(['users'], fromJS(users.map(user => [user._id, user])))
-  .mergeIn(['contacts'], fromJS(contacts.map(contact => [contact._id, contact])))
-  .mergeIn(['phones'], fromJS(phones.map(phone => [phone._id, phone])));
+  .mergeIn(['users', 'entities'], fromJS(users.map(user => [user._id, user])))
+  .mergeIn(['contacts', 'entities'], fromJS(contacts.map(contact => [contact._id, contact])))
+  .mergeIn(['phones', 'entities'], fromJS(phones.map(phone => [phone._id, phone])));
 
 describe('Graph reselect', () => {
-  it('should query graph data', () => {
-    const query = {
-      type: 'array',
-      getIn: ['users'],
-      filter: {},
-      map: {
-        contacts: {
-          getIn: ['contacts'],
-          filter: {},
-          map: {
-            phones: {
-              getIn: ['phones'],
-              filter: {},
-              map: {}
+  describe('querying', () => {
+    it('should query nested graph data', () => {
+      const query = {
+        type: 'array',
+        getIn: ['users', 'entities'],
+        filter: {},
+        map: {
+          contacts: {
+            getIn: ['contacts', 'entities'],
+            filter: {},
+            map: {
+              phones: {
+                getIn: ['phones', 'entities'],
+                filter: {},
+                map: {}
+              }
             }
           }
         }
-      }
-    };
+      };
 
-    const usersSelector = generateGraphSelector(query, {});
-    const users = usersSelector(state);
+      const usersSelector = generateGraphSelector(query, {});
+      const users = usersSelector(state);
+      const usersToCompare = fromJS([
+        {
+          _id: 'u1', name: 'User1', contacts: [
+            {
+              _id: 'c1', name: 'Contact 1', phones: [
+                { _id: 'p2', name: 'Phone 2' }
+              ]
+            },
+            {
+              _id: 'c3', name: 'Contact 3', phones: [
+                { _id: 'p4', name: 'Phone 4' }
+              ]
+            }
+          ]
+        },
+        {
+          _id: 'u2', name: 'User2', contacts: [
+            {
+              _id: 'c2', name: 'Contact 2', phones: [
+                {
+                  _id: 'p1', name: 'Phone 1'
+                },
+                {
+                  _id: 'p3', name: 'Phone 3'
+                }
+              ]
+            }
+          ]
+        }
+      ]);
 
-    const usersToCompare = fromJS([
-      {
-        _id: 'u1',
-        name: 'User1',
-        contacts: [
-          {
-            _id: 'c1',
-            name: 'Contact 1',
-            phones: [
-              {
-                _id: 'p2',
-                name: 'Phone 2'
-              }
-            ]
-          },
-          {
-            _id: 'c3',
-            name: 'Contact 3',
-            phones: [
-              {
-                _id: 'p4',
-                name: 'Phone 4'
-              }
-            ]
-          }
-        ]
-      },
-      {
-        _id: 'u2',
-        name: 'User2',
-        contacts: [
-          {
-            _id: 'c2',
-            name: 'Contact 2',
-            phones: [
-              {
-                _id: 'p1',
-                name: 'Phone 1'
-              },
-              {
-                _id: 'p3',
-                name: 'Phone 3'
-              }
-            ]
-          }
-        ]
-      }
-    ]);
+      expect(users.equals(usersToCompare)).to.be.true;
+    })
+  });
 
-    expect(users.equals(usersToCompare)).to.be.true
-  })
+  describe('filtering', () => {
+    it('should query with filter of type "value"', () => {
+      const query = {
+        type: 'array',
+        getIn: ['users', 'entities'],
+        filter: {
+          name: { type: 'value', value: 'User1' }
+        },
+        map: {}
+      };
+
+      const usersSelector = generateGraphSelector(query, {});
+      const users = usersSelector(state);
+      const usersToCompare = fromJS([
+        { _id: 'u1', name: 'User1', contacts: [ 'c1', 'c3' ] }
+      ]);
+
+      expect(users.equals(usersToCompare)).to.be.true;
+    });
+
+    it('should query with filter of type "param"', () => {
+      const query = {
+        type: 'array',
+        getIn: ['users', 'entities'],
+        filter: {
+          _id: { type: 'param', param: 'userId' }
+        },
+        map: {}
+      };
+
+      const usersSelector = generateGraphSelector(query, { userId: 'u1' });
+      const users = usersSelector(state);
+      const usersToCompare = fromJS([
+        { _id: 'u1', name: 'User1', contacts: [ 'c1', 'c3' ] }
+      ]);
+
+      expect(users.equals(usersToCompare)).to.be.true;
+    });
+
+    it('should query with filter of type "inArray"', () => {
+      const query = {
+        type: 'array',
+        getIn: ['contacts', 'entities'],
+        filter: {
+          _id: { type: 'inArray', param: 'contactIds' }
+        },
+        map: {}
+      };
+
+      const contactsSelector = generateGraphSelector(query, { contactIds: ['c1', 'c3'] });
+      const contacts = contactsSelector(state);
+      const contactsToCompare = fromJS([
+        { _id: 'c1', name: 'Contact 1', phones: ['p2'] },
+        { _id: 'c3', name: 'Contact 3', phones: ['p4'] }
+      ]);
+
+      expect(contacts.equals(contactsToCompare)).to.be.true;
+    });
+  });
 });
